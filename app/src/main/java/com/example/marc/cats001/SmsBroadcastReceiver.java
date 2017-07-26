be77cb89542d3739efdaede621320f52374c1ad1
Marc <mckeeverma@aol.com>
2017-07-26 00:30:51 -0400

    aa

--- app/src/main/java/com/example/marc/cats001/SmsBroadcastReceiver.java
+++ app/src/main/java/com/example/marc/cats001/SmsBroadcastReceiver.java
@@ -1,23 +1,23 @@
 package com.example.marc.cats001;
-
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
+import android.view.WindowManager;
 import android.widget.Toast;
 import android.telephony.SmsManager;
 
 import java.text.SimpleDateFormat;
-
+@SuppressWarnings("deprecation")
 public class SmsBroadcastReceiver extends BroadcastReceiver {
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
-
     public static final String SMS_BUNDLE = "pdus";
-
+    String TAG = "marclog";
     SmsMessage smsMessage;
-
     public void onReceive(Context context, Intent intent) {
         Bundle intentExtras = intent.getExtras();
         String format = intentExtras.getString("format");
@@ -25,7 +25,22 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
             Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
             String smsMessageStr = "";
             for (int i = 0; i < sms.length; ++i) {
-
+                Intent intent2 = new Intent();
+                intent2.setAction(Intent.ACTION_MAIN);
+                intent2.addCategory(Intent.CATEGORY_LAUNCHER);
+                intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK +
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED +
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD +
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON +
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+                intent2.setComponent(new ComponentName("com.example.marc.abc001", "com.example.marc.abc001.MainActivity"));
+                Log.d(TAG, "Calling startActivity now");
+                try {
+                    context.startActivity(intent2);
+                } catch (Exception e) {
+                    Log.d(TAG, "Error on startActivity: " + e.getMessage());
+                    e.printStackTrace();
+                }
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     smsMessage = SmsMessage.createFromPdu((byte[]) sms[i], format);
                 } else {
@@ -36,16 +51,13 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
                 Long messageDate = smsMessage.getTimestampMillis();
                 java.util.Date d = new java.util.Date(messageDate);
                 String itemDateStr = new SimpleDateFormat("dd-MMM HH:mm:ss").format(d);
-
                 smsMessageStr += "SMS From: " + address + "\n";
                 smsMessageStr += itemDateStr + "\n";
                 smsMessageStr += smsBody + "\n";
-
                 SmsManager smsManager = SmsManager.getDefault();
                 smsManager.sendTextMessage(address, null, "got it: " + smsBody, null, null);
             }
             Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
-
             //MainActivity inst = MainActivity.instance();
             //inst.updateList(smsMessageStr);
         }
